f0b6158fe232a64d64e4dfd2fdffcbd888222030
German Ponce <germanponce268@gmail.com>
2023-07-26 20:21:54 -0300

    test14

--- src/main/java/com/supportapp/domain/User.java
+++ src/main/java/com/supportapp/domain/User.java
@@ -15,6 +15,7 @@ import java.util.Date;
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
+        @Column(name = "userid")
         private String userId;
         @Column(name = "firstname")
         private String firstName;
