0116cf876b5c7c3e986a2f8f022c1533386bd3d0
German Ponce <germanponce268@gmail.com>
2023-07-26 19:57:33 -0300

    test11

--- src/main/java/com/supportapp/domain/User.java
+++ src/main/java/com/supportapp/domain/User.java
@@ -11,6 +11,7 @@ import java.util.Date;
     @Entity
     @Table( name = "user")
     public class User {
+        @Column(name = "id")
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
