e46c6c024dd5f5c22efa1828ff829c9320696d5d
German Ponce <germanponce268@gmail.com>
2023-07-28 17:06:51 -0300

    OPTIONS added

--- src/main/java/com/supportapp/constant/CorsConstant.java
+++ src/main/java/com/supportapp/constant/CorsConstant.java
@@ -3,7 +3,7 @@ package com.supportapp.constant;
 public class CorsConstant {
     public static final String[] ALLOWED_HEADERS = {"Origin", "Access-Control-Allow-Origin","Content-Type", "Accept", "Jwt-Token", "Authorization", "Origin, Accept","X-Requested-With","Access-Control-Request-Method", "Acces-Control-Request-Headers"};
     public static final String[] EXPOSED_HEADERS = {"Origin","Content-Type","Accept", "Jwt-Token", "Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"};
-    public static final String[] HTTP_METHODS = {"GET", "POST", "PUT", "DELETE"};
+    public static final String[] HTTP_METHODS = {"GET", "POST", "PUT", "DELETE", "OPTIONS"};
     public static final String SUPPORTAPPWEB_URL = "https://supportappweb.netlify.app/";
     public static final String SUPPORTAPPWEB_LOCAL = "http://localhost:4200";
 }
